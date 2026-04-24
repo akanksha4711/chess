0c20315762772672e7cce23d0cc715468c19bf20
Akanksha Sharma <akanksha@Akankshas-MacBook-Pro.local>
2026-04-24 12:59:09 +0530

    fix

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [URL || "http://localhost:5173"],
+    origin: [URL, "http://localhost:5173"],
     credentials: true,
   }),
 );
@@ -42,7 +42,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: [URL || "http://localhost:5173"],
+    origin: [URL, "http://localhost:5173"],
     credentials: true,
   },
 });
