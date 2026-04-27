259838a6ca15ff79f58879800d37fe091153acaa
Akanksha Sharma <akanksha@Akankshas-MacBook-Pro.local>
2026-04-27 19:19:34 +0530

    fix: added transport as polling for web sockets

--- backend/index.js
+++ backend/index.js
@@ -66,6 +66,7 @@ const io = new Server(server, {
     origin: CLIENT_URL || "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     credentials: true,
+    transports: ["polling"],
   },
 });
 
